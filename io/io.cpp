36b44d2e927a8c452aa3bf1bc74e26a06eef5d58
Pei Qing 卿培 <edwardtoday@gmail.com>
2014-04-19 11:18:52 +0800

    io

---
+++ io/io.cpp
@@ -0,0 +1,26 @@
+#include "io.hpp"
+
+namespace qingpei {
+namespace toolkit {
+namespace io {
+
+std::string read_file_as_string(const char* filename) {
+    // from http://stackoverflow.com/q/2602013
+    std::ifstream in(filename, std::ios::in | std::ios::binary);
+
+    if (in) {
+        std::string contents;
+        in.seekg(0, std::ios::end);
+        contents.resize(in.tellg());
+        in.seekg(0, std::ios::beg);
+        in.read(&contents[0], contents.size());
+        in.close();
+        return contents;
+    }
+
+    throw (errno);
+}
+
+} // namespace io
+} // namespace toolkit
+} // namespace qingpei
---
+++ io/io.hpp
@@ -0,0 +1,13 @@
+#ifndef QINGPEI_TOOLKIT_IO_
+#define QINGPEI_TOOLKIT_IO_
+
+namespace qingpei {
+namespace toolkit {
+namespace io {
+
+std::string read_file_as_string(const char* filename);
+
+} // namespace io
+} // namespace toolkit
+} // namespace qingpei
+#endif // QINGPEI_TOOLKIT_IO_
