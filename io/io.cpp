ada29bd554d1729d0e30901b25e9820ee2b987db
Pei Qing 卿培 <edwardtoday@gmail.com>
2014-04-22 17:14:58 +0800

    Format io.cpp

--- io/io.cpp
+++ io/io.cpp
@@ -5,20 +5,20 @@ namespace toolkit {
 namespace io {
 
 std::string read_file_as_string(const char* filename) {
-    // from http://stackoverflow.com/q/2602013
-    std::ifstream in(filename, std::ios::in | std::ios::binary);
+  // from http://stackoverflow.com/q/2602013
+  std::ifstream in(filename, std::ios::in | std::ios::binary);
 
-    if (in) {
-        std::string contents;
-        in.seekg(0, std::ios::end);
-        contents.resize(in.tellg());
-        in.seekg(0, std::ios::beg);
-        in.read(&contents[0], contents.size());
-        in.close();
-        return contents;
-    }
+  if (in) {
+    std::string contents;
+    in.seekg(0, std::ios::end);
+    contents.resize(in.tellg());
+    in.seekg(0, std::ios::beg);
+    in.read(&contents[0], contents.size());
+    in.close();
+    return contents;
+  }
 
-    throw (errno);
+  throw (errno);
 }
 
 } // namespace io
