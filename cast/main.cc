679b785e80f563e78a2fe5ba13d829b5496ca4ba
Pei Qing 卿培 <edwardtoday@gmail.com>
2014-04-26 14:09:15 +0800

    one more test case for cast

--- cast/main.cc
+++ cast/main.cc
@@ -2,7 +2,8 @@
 #include "assert.h"
 
 int main() {
-  uint8_t num15[] = {0x00, 0x0a};
-  assert(0x0a == qingpei::toolkit::cast::bytes_to_uint16(num15));
+  uint8_t bytes[] = {0x00, 0x0a, 0x01, 0x02};
+  assert(0x0a == qingpei::toolkit::cast::bytes_to_uint16(bytes));
+  assert(0x0a0102 == qingpei::toolkit::cast::bytes_to_uint32(bytes));
   return 0;
 }
